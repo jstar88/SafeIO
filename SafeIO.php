6e953bab3147cbec27f758489f1d878f6495fc15
jstar88 <covolo.n@libero.it>
2013-07-29 15:36:26 +0200

    clean up

--- SafeIO.php
+++ SafeIO.php
@@ -9,7 +9,6 @@
  */
 class SafeIO
 {
-    const WAIT_FOR = 200000;
     const PERMISSIONS = 0664;
 
     /**
@@ -20,20 +19,11 @@ class SafeIO
      */
     public static function open($path)
     {
-        if (!file_exists($path))
-        {
-            throw new Exception("file not exist " + $path);
-        }
-        if (!is_readable($path))
-        {
-            throw new Exception("file not readable " + $path);
-        }
+        if (!file_exists($path)) throw new Exception("file not exist " + $path);
+        if (!is_readable($path)) throw new Exception("file not readable " + $path);
         // Open for reading only; place the file pointer at the beginning of the file.
         $fo = fopen($path, 'r');
-        if ($fo === false)
-        {
-            throw new Exception("Error while opening the file " + $path);
-        }
+        if ($fo === false) throw new Exception("Error while opening the file " + $path);
         // acquire a shared lock
         if (flock($fo, LOCK_SH))
         {
@@ -43,10 +33,7 @@ class SafeIO
             chmod($path, self::PERMISSIONS);
             return $cts;
         }
-        else
-        {
-            throw new Exception("Error while trying to get lock at " + $path);
-        }
+        else throw new Exception("Error while trying to get lock at " + $path);      
     }
 
 
@@ -60,27 +47,19 @@ class SafeIO
      */
     public static function save($content, $path, $reset = true)
     {
-        if (file_exists($path) && !is_writable($path))
-        {
-            throw new Exception("File not writable at " + $path);
-        }
+        if (file_exists($path) && !is_writable($path)) throw new Exception("File not writable at " + $path);
         // Open for writing only; place the file pointer at the end of the file.
         // If the file does not exist, attempt to create it.
         // "w" option is not used to avoid the erase before locking.
         $fp = fopen($path, "a");
-        if ($fp === false)
-        {
-            throw new Exception("Error while opening the file " + $path);
-        }
+        if ($fp === false) throw new Exception("Error while opening the file " + $path);
         // acquire an exclusive lock or wait for it.
         if (flock($fp, LOCK_EX))
         {
             // if the file must be cleaned trunk him.
-            if ($reset)
-                ftruncate($fp, 0);
+            if ($reset) ftruncate($fp, 0);
             // write the contents.
-            if (fputs($fp, $content) === false)
-                throw new Exception("Error while saving contents at " + $path);
+            if (fputs($fp, $content) === false) throw new Exception("Error while saving contents at " + $path);
             // flush output before releasing the lock
             fflush($fp);
             // release the lock
@@ -88,10 +67,6 @@ class SafeIO
             fclose($fp);
             chmod($path, self::PERMISSIONS);
         }
-        else
-        {
-            throw new Exception("Error while trying to get lock at " + $path);
-        }
+        else throw new Exception("Error while trying to get lock at " + $path);
     }
-
 }
