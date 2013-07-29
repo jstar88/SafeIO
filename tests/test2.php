4b0bfe2861492e08d86f7d4354654f6bd3fb2f31
jstar88 <covolo.n@libero.it>
2013-07-29 23:14:49 +0200

    Update test2.php

--- tests/test2.php
+++ tests/test2.php
@@ -12,10 +12,13 @@ require('../SafeIO.php');
      
 ?>
 <html>
-<head><title>session race condition example</title></head>
+<head><title>race condition example</title></head>
 <body>
+  <p>This is a test to see the transaction at work.</p>
+  <p>click on the button "do stuff", while page is loading remove the code "sleep(20)" and change "right" to "wrong", then click in a new tab the sum button </p>
+  <p>You will see that the second script will wait for the end of first script</p>
   <form method="POST">
-    Delay caused by lock <?php echo time() - $time; ?> , max is 5.<br>
+    Delay caused by lock <?php echo time() - $time; ?> , max is 20.<br>
     <a><?php echo SafeIO::open("data.txt");?></a>
     
     <input type="submit" name="do_stuff" value="Do stuff!">
