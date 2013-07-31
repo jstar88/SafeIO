78320e82cbd3891de087864ef9be5f0cb6d128bf
jstar88 <covolo.n@libero.it>
2013-07-31 11:51:33 +0200

    comments

--- v2/ConcurrentFile.php
+++ v2/ConcurrentFile.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * SafeIO
- * Class that read and write content to a file
+ * Class that read and write content to a file in safe way.
  * @author Covolo Nicola
  * @copyright 2013
  * @license GNU v3
@@ -15,6 +15,12 @@ class ConcurrentFile
     private $handle;
     private $locks= array();
 
+    /**
+     * ConcurrentFile::__construct()
+     * Instantiate a new ConcurrentFile object.
+     * @param string $path
+     * @return ConcurrentFile
+     */
     public function __construct($path)
     {
         $this->path = $path;
@@ -23,6 +29,11 @@ class ConcurrentFile
         $this->locks[] = LOCK_UN;
     }
 
+    /**
+     * ConcurrentFile::close()
+     * Close the file and destroy the object.
+     * @return void
+     */
     public function close()
     {
         flock($this->handle, LOCK_UN);
@@ -30,12 +41,24 @@ class ConcurrentFile
         unset(self::$instances[$this->path]);
         chmod($this->path, self::PERMISSIONS);
     }
+    /**
+     * ConcurrentFile::__destruct()
+     * Magic method
+     * @return void
+     */
     public function __destruct()
     {
-        if(isset(self::$instances[$this->path]))
-            $this->close();
+        if(isset(self::$instances[$this->path])) $this->close();
     }
 
+    /**
+     * ConcurrentFile::write()
+     * Save the contents into a file located as the path.
+     * Specific in the second argument if the old content should be discarded.
+     * @param string $contents
+     * @param bool $reset
+     * @return ConcurrentFile
+     */
     public function write($contents, $reset = true)
     {
         $last = $this->locks[count($this->locks)-1];
@@ -54,6 +77,11 @@ class ConcurrentFile
         return $this;
     }
 
+    /**
+     * ConcurrentFile::read()
+     * Read the content from a file located as the path.
+     * @return string
+     */
     public function read()
     {
         if (!file_exists($this->path)) throw new Exception("File not exist at " . $this->path);
@@ -65,6 +93,11 @@ class ConcurrentFile
         return $contents;  
     }
     
+    /**
+     * ConcurrentFile::readLock()
+     * Start an shared lock: block others processes write.
+     * @return ConcurrentFile
+     */
     public function readLock()
     {
         if (!is_readable($this->path)) throw new Exception("File not readable at " . $this->path);
@@ -73,6 +106,11 @@ class ConcurrentFile
         return $this;
     }
 
+    /**
+     * ConcurrentFile::writeLock()
+     * Start an exclusive lock: block others processes from read and write.
+     * @return ConcurrentFile
+     */
     public function writeLock()
     {
         if (!is_writable($this->path))       throw new Exception("File not writable at " . $this->path);
@@ -81,6 +119,11 @@ class ConcurrentFile
         return $this;
     }
 
+    /**
+     * ConcurrentFile::releaseLock()
+     * Release the previous lock.
+     * @return ConcurrentFile
+     */
     public function releaseLock()
     {
         $last = array_pop($this->locks);
@@ -90,6 +133,12 @@ class ConcurrentFile
         return $this;   
     }
 
+    /**
+     * ConcurrentFile::getInstance()
+     * Search for active ConcurrentFile objects or instantiate it.
+     * @param string $path
+     * @return ConcurrentFile
+     */
     public static function getInstance($path)
     {
         if (!isset(self::$instances[$path]))
